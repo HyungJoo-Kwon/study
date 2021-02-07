714626d44107c9e4cc7b299d94ce2c8b486bf692
HyungJoo-Kwon <joo9437@naver.com>
2021-02-07 22:05:37 +0900

    commit

---
+++ opencv_python/images/04_top/reeeee.bat
@@ -0,0 +1,38 @@
+ren	1.bmp	1.bmp
+		
+ren	3.bmp	2.bmp
+ren	4.bmp	3.bmp
+ren	5.bmp	4.bmp
+		
+ren	7.bmp	5.bmp
+ren	8.bmp	6.bmp
+ren	9.bmp	7.bmp
+ren	10.bmp	8.bmp
+ren	11.bmp	9.bmp
+ren	12.bmp	10.bmp
+ren	13.bmp	11.bmp
+ren	14.bmp	12.bmp
+ren	15.bmp	13.bmp
+ren	16.bmp	14.bmp
+ren	17.bmp	15.bmp
+		
+ren	19.bmp	16.bmp
+ren	20.bmp	17.bmp
+ren	21.bmp	18.bmp
+ren	22.bmp	19.bmp
+ren	23.bmp	20.bmp
+ren	24.bmp	21.bmp
+ren	25.bmp	22.bmp
+ren	26.bmp	23.bmp
+		
+		
+ren	29.bmp	24.bmp
+ren	30.bmp	25.bmp
+ren	31.bmp	26.bmp
+		
+ren	33.bmp	27.bmp
+ren	34.bmp	28.bmp
+ren	35.bmp	29.bmp
+		
+ren	37.bmp	30.bmp
+
